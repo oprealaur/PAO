47270f8200f50c473132bab13bce620947b681ca
Oprea Laurentiu <oprea.laurentiu98@gmail.com>
2019-03-31 23:26:04 +0300

    Add files via upload

---
+++ Apartament.java
@@ -0,0 +1,23 @@
+package com.administratie;
+
+public class Apartament {
+    private int numar;
+    private float cota;
+    private int numarpers;
+
+    public void setCota(float cota) {
+        this.cota = cota;
+    }
+
+    public void setNumar(int numar) {
+        this.numar = numar;
+    }
+
+    public float getCota() {
+        return cota;
+    }
+
+    public int getNumar() {
+        return numar;
+    }
+}
